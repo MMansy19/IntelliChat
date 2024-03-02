452fa9e2f55f0fec966d313f9d9d750f7cadca09
abdomohamed96 <abdomohamed9192@gmail.com>
2024-03-02 23:34:13 +0200

    Add link of hosted frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,15 @@ import cors from "cors";
 config();
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://intellichat-mern.netlify.app');
+    // You can also specify multiple origins by separating them with commas
+    // res.setHeader('Access-Control-Allow-Origin', 'https://intellichat-mern.netlify.app, http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 //middlewares
 app.use(
     cors({ origin: "https://intellichat-mern.netlify.app", credentials: true })
